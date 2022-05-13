0c10cb83ff648e781b4afe7128db7f0d40b6e654
cmy <cmy2567337570@163.com>
2022-05-13 22:12:59 +0800

    feature1分支，修改了代码

--- 功能/index.js
+++ 功能/index.js
@@ -1,13 +1,1 @@
-let obj = "功能1";
-let obj = "功能2";
-
-
-
-// 这是我功能1独有的
-// 这是我功能1独有的
-// 这是我功能1独有的
-let obj = "功能2"
-
-const feature2 = "这是我功能2独有的功能";
-
-const feature3 = "这是附加的功能";
+let arr = [];   // feature1的代码
